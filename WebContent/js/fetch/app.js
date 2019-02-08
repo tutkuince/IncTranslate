6d5fc988d0f849a140ef46a74c73c92ad49ba40d
tutkuince <tutku.ince@outlook.com>
2019-02-08 15:19:49 +0300

    texts are formatted

--- WebContent/js/fetch/app.js
+++ WebContent/js/fetch/app.js
@@ -4,16 +4,25 @@ eventListeners();
 
 function eventListeners() {
     document.getElementById("translate-form").addEventListener("submit", translateText);
+    document.getElementById("language").onchange = function () {
+        // UI Process
+        ui.changeUI();
+    }
 }
+
+const ui = new UI();
 
 function translateText(e) {
     let language = document.getElementById("language").value;
     let word = document.getElementById("word").value;
 
-    const translate = new Translate(word, language);
-    translate.translateText()
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
-
+    if (word === "") {
+        alert("Input cannot be empty!");
+    } else {
+        const translate = new Translate(word, language);
+        translate.translateText()
+            .then(response => ui.displayTranslate(response))
+            .catch(err => console.error(err));
+    }
     e.preventDefault();
 }
