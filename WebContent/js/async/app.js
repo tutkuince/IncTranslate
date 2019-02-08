c48f007c1ad046e5f7dd72391a32ec1f17e2ba61
tutkuince <tutku.ince@outlook.com>
2019-02-08 15:37:19 +0300

    initialized translate object and translateText method

--- WebContent/js/async/app.js
+++ WebContent/js/async/app.js
@@ -10,5 +10,10 @@ function translateText(e) {
     const word = document.getElementById("word").value;
     const language = document.getElementById("language").value;
 
+    const translate = new Translate(word, language);
+    translate.translateText()
+        .then(response => console.log(response))
+        .catch(err => console.log(err));
+
     e.preventDefault();
 }
