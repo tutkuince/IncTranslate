0d0cb39b8835f87f92cffc6dba49564eebf1b007
tutkuince <tutku.ince@outlook.com>
2019-02-08 15:15:46 +0300

    construct UI class

---
+++ WebContent/js/fetch/ui.js
@@ -0,0 +1,11 @@
+class UI {
+    constructor() {
+        this.outputImage = document.getElementById("outputImage");
+        this.outputLanguage = document.getElementById("outputLanguage");
+        this.outputWord = document.getElementById("outputWord");
+
+        this.language = document.getElementById("language");
+    }
+
+
+}
