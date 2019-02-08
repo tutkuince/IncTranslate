def626a6396d74dcfe7083ed0b433a6aad103f5d
tutkuince <tutku.ince@outlook.com>
2019-02-08 15:26:16 +0300

    translateText()

--- WebContent/js/async/app.js
+++ WebContent/js/async/app.js
@@ -3,5 +3,12 @@
 eventListeners();
 
 function eventListeners() {
+    document.getElementById("translate-form").addEventListener("submit", translateText);
+}
 
+function translateText(e) {
+    const word = document.getElementById("word").value;
+    const language = document.getElementById("language").value;
+
+    e.preventDefault();
 }
