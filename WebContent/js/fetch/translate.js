eef409632ea761ba6f57d48f0761c9d78c67966c
tutkuince <tutku.ince@outlook.com>
2019-02-08 14:47:23 +0300

    translateText method

--- WebContent/js/fetch/translate.js
+++ WebContent/js/fetch/translate.js
@@ -4,3 +4,26 @@ class Translate {
         this.text = text;
         this.language = language;
     }
+
+    translateText() {
+        const endpoint = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=${this.apiKey}&text=${this.text}&lang=${this.language}`;
+        this.get(endpoint)
+            .then(data => {
+                return data.text[0];
+            }).catch(err => console.log(err));
+    }
+
+    get(url) {
+        return new Promise((resolve, reject) => {
+            fetch(url)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error("An error occurred!" + response.status);
+                    }
+                    return response.json();
+                })
+                .then(data => resolve(data))
+                .catch(err => reject(err));
+        });
+    }
+}
