b96ab59d765aedc7f1bc42ea8700af4139aba0be
tutkuince <tutku.ince@outlook.com>
2019-02-08 15:28:59 +0300

    translateText()

---
+++ WebContent/js/async/translate.js
@@ -0,0 +1,15 @@
+class Translate {
+    constructor(text, language) {
+        this.apiKey = "trnsl.1.1.20190206T162044Z.83c2a0a160da44dc.9b09b5d06777baf7ee2733aff889d6e3922c0570";
+        this.text = text;
+        this.language = language;
+    }
+
+    async translateText() {
+        const endpoint = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=${this.apiKey}&text=${this.text}&lang=${this.language}`;
+
+        const response = await fetch(endpoint);
+        const data = response.json();
+        return data;
+    }
+}
