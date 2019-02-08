1a535fd8baaec18740577706df169388f4499a29
tutkuince <tutku.ince@outlook.com>
2019-02-08 13:47:28 +0300

    translateText

--- WebContent/js/prototype/translate.js
+++ WebContent/js/prototype/translate.js
@@ -6,3 +6,27 @@ function Translate(text, language) {
     // XHR Object
     this.xhr = new XMLHttpRequest();
 }
+
+Translate.prototype.translateText = function (callback) {
+    // Ajax Processes
+
+    const endpoint = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=${this.apiKey}&text=${this.text}&lang=${this.language}`;
+
+    this.xhr.open("GET", endpoint, true);
+
+    this.xhr.onload = () => {
+        if (this.xhr.status) {
+            const json = JSON.parse(this.xhr.responseText);
+            const text = json.text[0];
+            callback(null, text);
+        } else
+            callback("An error occurred!", null);
+    };
+
+    this.xhr.send();
+};
+
+Translate.prototype.changeParameters =  function (newWorld, newLanguage) {
+    this.text = newWorld;
+    this.language = newLanguage;
+}
