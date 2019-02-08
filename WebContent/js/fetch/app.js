d87b1ed4bf6aa2191d0f1d84946d8e5470df4637
tutkuince <tutku.ince@outlook.com>
2019-02-08 14:55:40 +0300

    initialized translate object and used translateText method

--- WebContent/js/fetch/app.js
+++ WebContent/js/fetch/app.js
@@ -7,6 +7,13 @@ function eventListeners() {
 }
 
 function translateText(e) {
+    let language = document.getElementById("language").value;
+    let word = document.getElementById("word").value;
+
+    const translate = new Translate(word, language);
+    translate.translateText()
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
 
     e.preventDefault();
 }
